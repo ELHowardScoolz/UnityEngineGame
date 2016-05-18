1c9e22db7456e298cdf12938ef527e625c9fc223
ELHowardScoolz <Elijah@CSTAdmins-iMac-92.local>
2016-05-18 10:56:30 -0500

    Updated some small things
    Made a coin model and updated the canvas to include 3d objects as well
    as player movement

--- LongJohnSilver/Assets/Scripts/ReferenceTest.cs
+++ LongJohnSilver/Assets/Scripts/ReferenceTest.cs
@@ -4,7 +4,10 @@ using System.Collections;
 public class ReferenceTest : MonoBehaviour {
 
 	public float moveSpeed = 10;
+	public float jumpSpeed = 8.0f;
 	public float turnSpeed = 60;
+	public float gravity = 20.0f;
+	private Vector3 moveDir = Vector3.zero;
 
 	// Use this for initialization
 	void Start () {
@@ -13,10 +16,25 @@ public class ReferenceTest : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float hor = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
-		transform.Rotate(0, hor, 0);
+		CharacterController controller = GetComponent<CharacterController> ();
 
-		float ver = Input.GetAxis ("Vertical") * moveSpeed * Time.deltaTime;
-		transform.Translate(0, 0, ver);
+		if (controller.isGrounded) {
+			moveDir = new Vector3(Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			moveDir = transform.TransformDirection (moveDir);
+			moveDir *= moveSpeed;
+
+			//float ver = Input.GetAxis ("Vertical") * moveSpeed * Time.deltaTime;
+			//transform.Translate (0, 0, ver);
+
+			if (Input.GetButton ("Jump")) {
+				moveDir.y = jumpSpeed;
+			}
+		}
+
+		float hor = Input.GetAxis ("Horizontal") * turnSpeed * Time.deltaTime;
+		transform.Rotate (0, hor, 0);
+
+		moveDir.y -= gravity * Time.deltaTime;
+		controller.Move(moveDir * Time.deltaTime);
 	}
 }
